08c23486d6262aae0f3de344b54add7a4a9f082b
qianzhixiang <qianzhixiang@htsc.com>
2020-12-07 08:53:22 +0800

    修复State重复计算的bug

--- src/@State.ts
+++ src/@State.ts
@@ -5,23 +5,16 @@ export function State() {
   return (target: any, key: string) => {
     // 这里的target其实是被注解的类的prototype 是它的原型链，
     // 因此，如果要对原型的属性做点什么 得有个地方去存储
+    let value = Reflect.get(target, key);
     Object.defineProperty(target, key, {
       get() {
-        if (this !== target) {
-          // return value;
-          const descriptor = Object.getOwnPropertyDescriptor(this, key);
-          // 这表示这个对象没有被代理过， 我来代理了
-          if (!descriptor) {
-            return Reflect.get(target, key);
-          }
-        }
-        return Reflect.get(target, key);
+        return value;
       },
       set(v) {
         // 这表示自己可能作为了别人的原型链的一员，这时不应该做什么事
         const descriptor = Object.getOwnPropertyDescriptor(this, key);
         if (this === target || descriptor) {
-          Reflect.set(target, key, v);
+          value = v;
         }
         if (!descriptor) {
           let tValue = v;
